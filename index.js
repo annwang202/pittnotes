b55ad8f5582dbbc0b9b5e34c9cfd423df09c46bc
annwang202 <89858886+annwang202@users.noreply.github.com>
2023-02-04 00:04:02 -0500

    Update index.js

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ function initMap() {
 function addMarker(location, map) {
   // Add the marker at the clicked location, and add the next-available label
   // from the array of alphabetical characters.
-  const marker = new google.maps.Marker({
+  var marker = new google.maps.Marker({
     position: location,
     label: labels[labelIndex++ % labels.length],
     map: map,
