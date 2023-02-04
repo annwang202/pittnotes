d711c3ba9bc5bb30034f51e04d2d7cbbaba9aae1
cashstation <95110043+cashstation@users.noreply.github.com>
2023-02-04 11:19:42 -0500

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function initMap() {
   var map = new google.maps.Map(document.getElementById("map"), {
     // Create the DIV to hold the control and call the makeInfoBox() constructor
     // passing in this DIV.
-    zoom: 20,
+    zoom: 4,
     center: pittsburgh,
     disableDoubleClickZoom: true,
   });
