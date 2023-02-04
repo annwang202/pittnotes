90dbb55f37e50b11f469dbc72cb7ee66b61561b1
annwang202 <89858886+annwang202@users.noreply.github.com>
2023-02-03 23:24:59 -0500

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,25 @@ function initMap() {
 }
 
 window.initMap = initMap;
+
+function makeInfoBox(controlDiv, map) {
+  // Set CSS for the control border.
+  var controlUI = document.createElement('div');
+  controlUI.style.boxShadow = 'rgba(0, 0, 0, 0.298039) 0px 1px 4px -1px';
+  controlUI.style.backgroundColor = '#fff';
+  controlUI.style.border = '2px solid #fff';
+  controlUI.style.borderRadius = '2px';
+  controlUI.style.marginBottom = '22px';
+  controlUI.style.marginTop = '10px';
+  controlUI.style.textAlign = 'center';
+  controlDiv.appendChild(controlUI);
+
+  // Set CSS for the control interior.
+  var controlText = document.createElement('div');
+  controlText.style.color = 'rgb(25,25,25)';
+  controlText.style.fontFamily = 'Roboto,Arial,sans-serif';
+  controlText.style.fontSize = '100%';
+  controlText.style.padding = '6px';
+  controlText.innerText = 'The map shows all clicks made in the last 10 minutes.';
+  controlUI.appendChild(controlText);
+}
