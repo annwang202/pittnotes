9728848f3789a5e49fadc4c0aa808fca4bf96a47
LSC46 <104258062+LSC46@users.noreply.github.com>
2023-02-04 17:30:55 -0500

    Create build-table.js

---
+++ build-table.js
@@ -0,0 +1,7 @@
+let mountains = [
+  { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Falterona", height: 1654, place: "Parco Foreste Casentinesi" },
+  { name: "Poggio Scali", height: 1520, place: "Parco Foreste Casentinesi" },
+  { name: "Pratomagno", height: 1592, place: "Parco Foreste Casentinesi" },
+  { name: "Monte Amiata", height: 1738, place: "Siena" }
+];
