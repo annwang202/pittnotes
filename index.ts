2441fad8b0d22aac3885b35934b4bb4c93032dd6
cashstation <95110043+cashstation@users.noreply.github.com>
2023-02-04 11:16:56 -0500

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,19 +1,20 @@
 // Initialize and add the map
 function initMap(): void {
-  // The location of Uluru
-  const uluru = { lat: -25.344, lng: 131.031 };
-  // The map, centered at Uluru
+  // The location of pittsburgh
+  //40.440644181300755, -80.00708798118193
+  const pitt = { lat: 40.44, lng: -80.007};
+  // The map, centered at pittsburgh
   const map = new google.maps.Map(
     document.getElementById("map") as HTMLElement,
     {
       zoom: 4,
-      center: uluru,
+      center: pitt,
     }
   );
 
   // The marker, positioned at Uluru
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: pitt,
     map: map,
   });
 }
