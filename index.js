926440bb726caba065a81b08fbc2f8f84c73965d
LSC46 <104258062+LSC46@users.noreply.github.com>
2023-02-04 19:10:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -76,6 +76,7 @@ function initMap() {
     console.log(numMarkers);
     console.log(markers);
     var tableInf = {Category: category, Event: eventName, Date: time, Where: place, About: eventNote};
+     tableobjects.push(tableInf);
     addToSection(category,labelContent);
     //addToTable(labelContent);
   });
@@ -151,4 +152,41 @@ function makeInfoBox(controlDiv, map) {
   controlText.style.padding = '6px';
   controlText.innerText = 'The map shows all clicks made in the last 10 minutes.';
   controlUI.appendChild(controlText);
+}
+
+{
+
+ let tableobjects = [
+  { name: "Monte Falco", height: 1658, place: "Parco Foreste Casentinesi",time: 6 },
+];
+
+
+function generateTableHead(table, data) {
+  let thead = table.createTHead();
+  let row = thead.insertRow();
+  for (let key of data) {
+    let th = document.createElement("th");
+    let text = document.createTextNode(key);
+    th.appendChild(text);
+    row.appendChild(th);
+  }
+}
+
+function generateTable(table, data) {
+  for (let element of data) {
+    let row = table.insertRow();
+    for (key in element) {
+      let cell = row.insertCell();
+      let text = document.createTextNode(element[key]);
+      cell.appendChild(text);
+    }
+  }
+}
+
+let table = document.querySelector("table");
+let data = Object.keys(tableobjects[0]);
+generateTableHead(table, data);
+generateTable(table, tableobjects);
+
+
 }
